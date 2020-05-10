9c7ddaf4da6318215cc8347bdeba3781ae856532
gamerAK101 <perceptivelifes@gmail.com>
2020-05-09 19:25:57 -0700

    My calculator

---
+++ Scripts/calculator.js
@@ -0,0 +1,32 @@
+let add = () =>{
+    let n1=10;
+    let n2=5;
+    let total = n1 + n2
+    console.log(total)
+}
+
+let sub = () =>{
+    let n1=10;
+    let n2=5;
+    let total = n1 - n2
+    console.log(total)
+}
+
+let mul = () =>{
+    let n1=10;
+    let n2=5;
+    let total= n1 * n2
+    console.log(total)
+}
+
+let div = () =>{
+    let n1 = 10;
+    let n2 = 5;
+    let total = n1 / n2
+    console.log(total)
+}
+
+add()
+sub()
+mul()
+div()
